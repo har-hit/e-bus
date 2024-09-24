81d47bcf329aacb198fbbf1a44633a7a2415b3fe
har-hit <harshithreddy0292@gmail.com>
2024-09-24 16:13:00 +0530

    Add files via upload

---
+++ data_driven_energy_economy_prediction/Remote_User/views.py
@@ -0,0 +1,239 @@
+from django.db.models import Count
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
+# Create your views here.
+from Remote_User.models import ClientRegister_Model,energy_economy_prediction,detection_ratio,detection_accuracy
+
+def login(request):
+
+
+    if request.method == "POST" and 'submit1' in request.POST:
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            enter = ClientRegister_Model.objects.get(username=username,password=password)
+            request.session["userid"] = enter.id
+
+            return redirect('ViewYourProfile')
+        except:
+            pass
+
+    return render(request,'RUser/login.html')
+
+def index(request):
+    return render(request, 'RUser/index.html')
+
+def Add_DataSet_Details(request):
+
+    return render(request, 'RUser/Add_DataSet_Details.html', {"excel_data": ''})
+
+
+def Register1(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        phoneno = request.POST.get('phoneno')
+        country = request.POST.get('country')
+        state = request.POST.get('state')
+        city = request.POST.get('city')
+        address = request.POST.get('address')
+        gender = request.POST.get('gender')
+        ClientRegister_Model.objects.create(username=username, email=email, password=password, phoneno=phoneno,
+                                            country=country, state=state, city=city,address=address,gender=gender)
+
+        obj = "Registered Successfully"
+        return render(request, 'RUser/Register1.html',{'object':obj})
+    else:
+        return render(request,'RUser/Register1.html')
+
+def ViewYourProfile(request):
+    userid = request.session['userid']
+    obj = ClientRegister_Model.objects.get(id= userid)
+    return render(request,'RUser/ViewYourProfile.html',{'object':obj})
+
+
+def Predict_Energy_Economy_Type(request):
+    if request.method == "POST":
+
+        if request.method == "POST":
+
+            Fid=request.POST.get('Fid')
+            Brand=request.POST.get('Brand')
+            Model=request.POST.get('Model')
+            AccelSec=request.POST.get('AccelSec')
+            TopSpeed_KmH=request.POST.get('TopSpeed_KmH')
+            Range_Km=request.POST.get('Range_Km')
+            Efficiency_WhKm=request.POST.get('Efficiency_WhKm')
+            FastCharge_KmH=request.POST.get('FastCharge_KmH')
+            RapidCharge=request.POST.get('RapidCharge')
+            PowerTrain=request.POST.get('PowerTrain')
+            PlugType=request.POST.get('PlugType')
+            BodyStyle=request.POST.get('BodyStyle')
+            Segment=request.POST.get('Segment')
+            Charging_Price=request.POST.get('Charging_Price')
+
+
+        df = pd.read_csv('Datasets.csv',encoding='latin-1')
+
+        def apply_response(Label):
+            if (Label == 0):
+                return 0  # Low
+            elif(Label ==1):
+                return 1  # High
+
+
+        df['results'] = df['Label'].apply(apply_response)
+
+        cv = CountVectorizer()
+        X = df['Fid'].apply(str)
+        y = df['results']
+
+        print("Fid")
+        print(X)
+        print("Results")
+        print(y)
+
+        cv = CountVectorizer()
+        X = cv.fit_transform(X)
+
+        models = []
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+        X_train.shape, X_test.shape, y_train.shape
+
+        print("Naive Bayes")
+        from sklearn.naive_bayes import MultinomialNB
+        NB = MultinomialNB()
+        NB.fit(X_train, y_train)
+        predict_nb = NB.predict(X_test)
+        naivebayes = accuracy_score(y_test, predict_nb) * 100
+        print(naivebayes)
+        print(confusion_matrix(y_test, predict_nb))
+        print(classification_report(y_test, predict_nb))
+        models.append(('naive_bayes', NB))
+
+
+        # SVM Model
+        print("SVM")
+        from sklearn import svm
+        lin_clf = svm.LinearSVC()
+        lin_clf.fit(X_train, y_train)
+        predict_svm = lin_clf.predict(X_test)
+        svm_acc = accuracy_score(y_test, predict_svm) * 100
+        print(svm_acc)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, predict_svm))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, predict_svm))
+        models.append(('svm', lin_clf))
+
+
+        print("Logistic Regression")
+
+        from sklearn.linear_model import LogisticRegression
+        reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+        y_pred = reg.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, y_pred) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, y_pred))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, y_pred))
+        models.append(('logistic', reg))
+
+
+        print("KNeighborsClassifier")
+        from sklearn.neighbors import KNeighborsClassifier
+        kn = KNeighborsClassifier()
+        kn.fit(X_train, y_train)
+        knpredict = kn.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, knpredict) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, knpredict))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, knpredict))
+        models.append(('KNeighborsClassifier', kn))
+
+
+        print("Random Forest Classifier")
+        from sklearn.ensemble import RandomForestClassifier
+        rf_clf = RandomForestClassifier()
+        rf_clf.fit(X_train, y_train)
+        rfpredict = rf_clf.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, rfpredict) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, rfpredict))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, rfpredict))
+        models.append(('RandomForestClassifier', rf_clf))
+
+
+        print("Artificial Neural Network (ANN)")
+
+        from sklearn.neural_network import MLPClassifier
+        mlpc = MLPClassifier().fit(X_train, y_train)
+        y_pred = mlpc.predict(X_test)
+        print("ACCURACY")
+        print(accuracy_score(y_test, y_pred) * 100)
+        print("CLASSIFICATION REPORT")
+        print(classification_report(y_test, y_pred))
+        print("CONFUSION MATRIX")
+        print(confusion_matrix(y_test, y_pred))
+        models.append(('MLPClassifier', mlpc))
+
+
+        classifier = VotingClassifier(models)
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+
+        Fid1 = [Fid]
+        vector1 = cv.transform(Fid1).toarray()
+        predict_text = classifier.predict(vector1)
+
+        pred = str(predict_text).replace("[", "")
+        pred1 = pred.replace("]", "")
+
+        prediction = int(pred1)
+
+        if (prediction == 0):
+            val = 'Low'
+        elif (prediction == 1):
+            val = 'High'
+
+        print(val)
+        print(pred1)
+
+        energy_economy_prediction.objects.create(
+        Fid=Fid,
+        Brand=Brand,
+        Model=Model,
+        AccelSec=AccelSec,
+        TopSpeed_KmH=TopSpeed_KmH,
+        Range_Km=Range_Km,
+        Efficiency_WhKm=Efficiency_WhKm,
+        FastCharge_KmH=FastCharge_KmH,
+        RapidCharge=RapidCharge,
+        PowerTrain=PowerTrain,
+        PlugType=PlugType,
+        BodyStyle=BodyStyle,
+        Segment=Segment,
+        Charging_Price=Charging_Price,
+        Prediction=val)
+
+        return render(request, 'RUser/Predict_Energy_Economy_Type.html',{'objs': val})
+    return render(request, 'RUser/Predict_Energy_Economy_Type.html')
+
+
+
